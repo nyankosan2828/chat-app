f520d92be6fd26c953902b45fb78aecb6ec21c42
nyankosan2828 <69426088+nyankosan2828@users.noreply.github.com>
2020-08-17 13:09:36 +0900

    deviseに関するビューファイルの配置

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_122931) do
+ActiveRecord::Schema.define(version: 2020_08_17_034250) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
