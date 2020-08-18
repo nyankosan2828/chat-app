01b4bf7d6ac5256cb75728e8528f32d37e44eaf4
nyankosan2828 <69426088+nyankosan2828@users.noreply.github.com>
2020-08-18 22:21:38 +0900

    各モデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable,  :validatable
-
          has_many :room_users
          has_many :rooms, through: :room_users
          has_many :messages
